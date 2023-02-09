b643126bca87fb46f65eb00ad5f87b7e293f0f91
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-02-09 14:06:49 -0500

    feat: CategoryController added with get list

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using ErpProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpProject.Controllers;
+
+[ApiController]
+[Route("api/v1/categories")]
+public class CategoryController : ControllerBase
+{
+    private readonly ILogger<Category> _logger;
+
+    private static List<Category> ListCategory = new ();
+
+    public CategoryController(ILogger<Category> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public IEnumerable<Category> Get()
+    {
+        return ListCategory;
+    }
+}
