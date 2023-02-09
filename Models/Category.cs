56430ae1345f8f3537b70c05d29c295f18a4e7a2
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-02-09 00:28:29 -0500

    feat: Category models and migrations added

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpProject.Models;
+
+public class Category
+{
+    public long CategoryId { get; set; }
+    public string? Name { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
