e096d8ac22c1e0e955c6512ffaea4856d273a7bb
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-02-09 00:27:48 -0500

    feat: DataContext to manage database configurations added

---
+++ DataContext.cs
@@ -0,0 +1,33 @@
+﻿using ErpProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpProject;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.HasKey(c => c.CategoryId);
+            category.Property(c => c.Name).HasMaxLength(255);
+            category.Property(c => c.IsActive);
+            category.Property(c => c.CreatedAt);
+            category.Property(c => c.UpdatedAt);
+        });
+    }
+}
